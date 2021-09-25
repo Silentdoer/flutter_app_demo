e6922ea0b1536cbf99bc7f6dafc08046ed9000d2
Silentdoer <1010993610@qq.com>
2021-09-25 21:18:05 +0800

    navbar create

---
+++ lib/navbar/navbar_factory.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:resize/resize.dart';
+
+class NavbarFactory {
+  NavbarFactory._();
+
+  static PreferredSizeWidget searchNavbar(String placeholder,
+      {TextAlign textAlign = TextAlign.center,
+      TextAlignVertical textAlignVertical = TextAlignVertical.center,
+      List<Widget>? actions}) {
+    return AppBar(
+      centerTitle: true,
+      title: SizedBox(
+          width: 80.vw,
+          child: CupertinoTextField(
+            
+            placeholder: placeholder,
+            textAlign: textAlign,
+            textAlignVertical: textAlignVertical,
+            // 光标在非iOS平台垂直对齐文字【光标垂直居中在web上有问题，但是在windows上可以，所以也可以理解为是flutter的锅】
+            // height高度是值乘以fontSize
+            style: const TextStyle(height: 1.2, textBaseline: TextBaseline.ideographic),
+          )),
+      actions: actions,
+    );
+  }
+}
